46f0d852babafeab3af8d6f3dd5569f3edd6dc8e
Krasavchik <mhailtnko@gmail.com>
2024-02-10 11:53:32 +0100

    git ignore and api key

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+
+
+headers = {
+    'user-agent': 'Michael'
+}
+with open('api key.txt' 'r') as key:
+    key = key
+
+
+r = request.get('https://my-api-url', headers=headers)
+
 
